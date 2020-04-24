81a26e0624027a4b2760107b6920bca74378cb56
thexdev <akbarnugroho253@gmail.com>
2020-04-24 20:27:24 +0700

    created "theme.js"

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+import { theme } from '@chakra-ui/core';
+
+const breakpoints = ['360px', '768px', '1024px', '1440px'];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[4];
+
+export default {
+  ...theme,
+  breakpoints,
+  fonts: {
+    heading: '"Avenir Next", sans-serif',
+    body: 'system-ui, sans-serif',
+    mono: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '4rem',
+  },
+};
