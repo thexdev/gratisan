ae3218405b72d5e36c7c155d5d9dbb22e55274fb
thexdev <akbarnugroho253@gmail.com>
2020-04-25 23:52:14 +0700

    created "About" section

---
+++ src/pages/home/sections/About.js
@@ -0,0 +1,63 @@
+import React, { memo } from 'react';
+import Section from 'components/section';
+import { Box, Button, SimpleGrid, Text } from '@chakra-ui/core';
+import { Link } from 'react-router-dom';
+
+const columns = {
+  sm: 1,
+  md: 2,
+};
+
+const About = () => {
+  return (
+    <Section heading="GRATISAN">
+      <SimpleGrid columns={columns} spacing={10}>
+        <Box></Box>
+        <Box>
+          <Text fontSize="xl">
+            Jika kamu adalah seorang developer atau programmer,{' '}
+            <Text as="strong" color="purple.600">
+              Gratisan
+            </Text>{' '}
+            adalah tempat dimana kamu bisa menemukan resource dan layanan yang
+            bisa kamu gunakan secara gratis
+          </Text>
+          <Text fontSize="xl" mt="0.7em">
+            Resource dan layanan yang bisa kamu temukan adalah{' '}
+            <Text as="strong" color="purple.600">
+              cloud computing
+            </Text>
+            ,{' '}
+            <Text as="strong" color="purple.600">
+              open api
+            </Text>
+            ,{' '}
+            <Text as="strong" color="purple.600">
+              font
+            </Text>{' '}
+            dan masih banyak lagi!
+          </Text>
+          <Text fontSize="xl" mt="0.7em">
+            Kamu bisa membuat aplikasi atau service dari hal tersebut. Atau
+            sekedar belajar biar makin jago!
+          </Text>
+          <Link to="/tentang">
+            <Button
+              mt="1.3em"
+              bg="purple.500"
+              color="gray.100"
+              _hover={{
+                bg: 'gray.100',
+                color: 'red.400',
+              }}
+            >
+              Baca selengkapnya
+            </Button>
+          </Link>
+        </Box>
+      </SimpleGrid>
+    </Section>
+  );
+};
+
+export default memo(About);
